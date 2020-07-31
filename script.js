06c42857c950f69457729b979aee18a0cc1a474a
Aragon <jearagon777Gmail.com>
2020-07-30 17:09:53 -0700

    did some css, and started my java

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-document.querySelector("button").addEventListener("click")
+document.querySelector("button").addEventListener("click");
+var quiz = document.querySelector("#quiz")
+var questions = [{
+
+    // i want an new background image for every questin-img src="",
+    question: "Who is the greatest lightsaber dualist of all time?",
+    answers: ['Yoda', 'Anakin Skywalker', 'Mace Windu', 'Darth Sidius'],
+    correct: 'Mace Windu'
+},
+    
+]
