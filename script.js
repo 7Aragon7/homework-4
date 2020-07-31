d7da8dd99c400ed3e1eae964ec174c43d0c5c985
Aragon <jearagon777Gmail.com>
2020-07-30 17:35:15 -0700

    java

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.querySelector("button").addEventListener("click");
-var quiz = document.querySelector("#quiz")
+var main = document.querySelector("#main");
+var quiz = document.querySelector("#quiz");
 var questions = [{
 
     // i want an new background image for every questin-img src="",
@@ -7,5 +8,28 @@ var questions = [{
     answers: ['Yoda', 'Anakin Skywalker', 'Mace Windu', 'Darth Sidius'],
     correct: 'Mace Windu'
 },
-    
+{
+    // img src goes here
+    question: "What is Darth Sidus home planet?",
+    answers: ['Coruscant', 'Naboo', 'Muuniinst', 'Tatoomie'],
+    correct: 'Naboo'
+},
+{
+    // img src goes here
+    question: "Who's lightasber does Luke usse in episode 4-5?",
+    answers: ["Darth Vader's", "Obi Won Kenobi's", "Anakin Skywalker's","Yoda's"],
+    correct: "Anakin Skywalker's"
+},
+{
+    // img src goes here
+    question: "who kills Emperor Palpatine?",
+    answers: ["Luke Skywalker", "Darth Vader", "Rey", "Kilo Ren"],
+    correct: "Darth Vader"
+}
 ]
+
+var currentQuestions = 0;
+
+function startquiz() {
+    main.style = "display:none";
+}
